73b7aaaa455a45bfd4e6110087c8fbb295eb528c
İbrahim Yegen <yegenibrahim@beycelikgestamp.com.tr>
2024-10-17 14:38:11 +0300

    different name property type

--- AspWebProject/DTOs/CustomerDto.cs
+++ AspWebProject/DTOs/CustomerDto.cs
@@ -3,8 +3,8 @@
     public class CustomerDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public int Age { get; set; }
+        public string Isim { get; set; }
+        public string Eposta { get; set; }
+        public int Yas { get; set; }
     }
 }
