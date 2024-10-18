c385347ed62d70e63d0e5dfc0b2a28f5a03634b6
İbrahim Yegen <yegenibrahim@beycelikgestamp.com.tr>
2024-10-18 11:24:55 +0300

    Create projection event

---
+++ AspWebProject/Controllers/ProjectionController.cs
@@ -0,0 +1,31 @@
+﻿using AspWebProject.DTOs;
+using AspWebProject.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspWebProject.Controllers
+{
+    public class ProjectionController : Controller
+    {
+        private readonly IMapper _mapper;
+
+        public ProjectionController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(EventDateDto eventDateDto)
+        {
+            EventDate eventDate= _mapper.Map<EventDate>(eventDateDto);
+
+
+            ViewBag.Date= eventDate.Date.ToShortDateString();   
+            return View();
+        }
+    }
+}
