2e6e4b467d98602412876ce4bed44064dc5c5920
İbrahim Yegen <yegenibrahim@beycelikgestamp.com.tr>
2024-10-18 09:51:04 +0300

    İnclude Members Method

--- AspWebProject/DTOs/CustomerDto.cs
+++ AspWebProject/DTOs/CustomerDto.cs
@@ -2,18 +2,18 @@
 {
     public class CustomerDto
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public int Age { get; set; }
-
         //public int Id { get; set; }
-        //public string Isim { get; set; }
-        //public string Eposta { get; set; }
-        //public int Yas { get; set; }
+        //public string Name { get; set; }
+        //public string Email { get; set; }
+        //public int Age { get; set; }
+
+        public int Id { get; set; }
+        public string Isim { get; set; }
+        public string Eposta { get; set; }
+        public int Yas { get; set; }
         public string  FullName { get; set; }
 
-        public string CreditCardNumber { get; set; }
-        public DateTime CreditCardValidDate { get; set; }
+        public string Number { get; set; }
+        public DateTime ValidDate { get; set; }
     }
 }
--- AspWebProject/Mapping/MapProfile.cs
+++ AspWebProject/Mapping/MapProfile.cs
@@ -10,17 +10,18 @@ namespace AspWebProject.Mapping
         {
             // normal mapping işlemi
 
-            CreateMap<Customer, CustomerDto>().ReverseMap();
+            //CreateMap<Customer, CustomerDto>().ReverseMap();
 
 
             //eğer alanlar farklı isimlerde ise bu sekilde maplama yapılır  bu yüzden aynı isimleri vermeye dikkat etmekte fayda var!!
 
+            CreateMap<CreditCard, CustomerDto>();
 
-            //CreateMap<Customer, CustomerDto>()
-            //    .ForMember(dest => dest.Isim, opt => opt.MapFrom(dest => dest.Name))
-            //    .ForMember(dest => dest.Eposta, opt => opt.MapFrom(dest => dest.Email))
-            //    .ForMember(dest => dest.Yas, opt => opt.MapFrom(dest => dest.Age))
-            //    .ForMember(dest => dest.FullName, opt => opt.MapFrom(dest => dest.GetFullName()));
+            CreateMap<Customer, CustomerDto>().IncludeMembers(x=>x.CreditCard)
+                .ForMember(dest => dest.Isim, opt => opt.MapFrom(dest => dest.Name))
+                .ForMember(dest => dest.Eposta, opt => opt.MapFrom(dest => dest.Email))
+                .ForMember(dest => dest.Yas, opt => opt.MapFrom(dest => dest.Age))
+                .ForMember(dest => dest.FullName, opt => opt.MapFrom(dest => dest.GetFullName()));
         }
     }
 }
