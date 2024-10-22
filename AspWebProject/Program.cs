968f546b2be17a720b3df0e98f526932a108d0fe
İbrahim Yegen <yegenibrahim@beycelikgestamp.com.tr>
2024-10-22 09:33:19 +0300

    IP Rate Limit

--- AspWebProject/Program.cs
+++ AspWebProject/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 
 builder.Services.AddAutoMapper(typeof(MapProfile));
+builder.Services.AddAutoMapper(typeof(EventDateProfile));
 
 
 var app = builder.Build();
---
+++ RateLimit.API/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimit.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetCustomer()
+        {
+            return Ok(new { Id = 1, Name = "Ford Otosan", Date = DateTime.Now.ToString() });
+        }
+    }
+}
