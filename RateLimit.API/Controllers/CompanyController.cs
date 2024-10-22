2c1469c4c6a3bd57138db49e171e3719fc6da006
İbrahim Yegen <yegenibrahim@beycelikgestamp.com.tr>
2024-10-22 09:57:49 +0300

    One more controller settings on appsettings.json

---
+++ RateLimit.API/Controllers/CompanyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimit.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase  
+    {
+        [HttpGet]
+        public IActionResult GetCompany()
+        {
+            return Ok(new { Id = 1, Name = "Tesla", Date = DateTime.Now.ToString() });
+        }
+
+        [HttpPost]
+        public IActionResult SaveCompany()
+        {
+            return Ok(new { Id = 1, CompanyName = "Renault", Date = DateTime.Now.ToString() });
+        }
+    }
+}
